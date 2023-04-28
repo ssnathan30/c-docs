024cb9b2347904f9a65ef0a3c4adf54ed0932fb8
Swaminathan Sriram <thedarkknight@Swaminathans-MacBook-Pro.local>
2023-04-28 02:10:04 -0600

    Initial commit

---
+++ sync-server/server.py
@@ -0,0 +1,35 @@
+import flask
+from flask_cors import CORS
+import pathlib
+import os
+
+app = flask.Flask("databoy")
+CORS(app)
+
+@app.route("/<path:filename>", methods=["GET"])
+def get_file(filename):
+    path: pathlib.Path = pathlib.Path("data") / filename
+    if not path.exists():
+        return  flask.make_response("not found", 404)
+    return flask.send_from_directory("./data", filename, cache_timeout=0)
+
+
+@app.route("/<path:filename>", methods=["POST"])
+def upload_file(filename):
+    with open(f"./data/{filename}", "wb") as outfile:
+        data = flask.request.stream.read()
+        outfile.write(data)
+    return "done"
+
+@app.route("/<path:filename>", methods=["DELETE"])
+def delete_file(filename):
+    path: pathlib.Path = pathlib.Path("data") / filename
+    if not path.exists():
+        return  flask.make_response("not found", 404)
+    os.remove(f"./data/{filename}")
+    return "delete done"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
